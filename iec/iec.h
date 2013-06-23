56b5b931d3189c9d2f6d5c8599b405a77ae6a42f
Chris Osborn <fozztexx@fozztexx.com>
2013-06-23 23:53:43 +0000

    Moved IEC header to iec.h.

---
+++ iec/iec.h
@@ -0,0 +1,26 @@
+/* Copyright 2013 by Chris Osborn <fozztexx@fozztexx.com>
+ *
+ * This file is part of ninepin.
+ *
+ * ninepin is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * ninepin is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ninepin; see the file COPYING. If not see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <linux/types.h>
+
+typedef struct {
+  unsigned char command;
+  unsigned char channel;
+  uint16_t len;
+} iec_data;
