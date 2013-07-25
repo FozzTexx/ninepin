c1e64fc1f0839708683af3fc685e38a30921a41b
Chris Osborn <fozztexx@fozztexx.com>
2013-07-24 19:53:07 -0700

    Directory listings work.

--- cbmdos.c
+++ cbmdos.c
@@ -23,6 +23,137 @@
 #include <string.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <ctype.h>
+
+void dosMakeC64Name(const char *original, char *c64)
+{
+  static char *buf = NULL;
+  static int buflen = 0;
+  int len, elen;
+  char *exten;
+  int i;
+
+
+  len = strlen(original);
+  if (len+1 > buflen) {
+    buflen = ((len + 32) / 32) * 32;
+    buf = realloc(buf, len);
+  }
+  strcpy(buf, original);
+
+  /* FIXME - deal with unofficial UTF8 PETSCII mapping */  
+  for (i = 0; i < len; i++)
+    buf[i] = toupper(buf[i]);
+  if (!(exten = strrchr(buf, '.'))) {
+    elen = len;
+    exten = "USR";
+  }
+  else {
+    elen = exten - buf;
+    exten++;
+
+    if (!strcmp(exten, "TXT"))
+      strcpy(exten, "SEQ");
+
+    if (strcmp(exten, "SEQ") && strcmp(exten, "USR") &&	strcmp(exten, "PRG")) {
+      elen = len;
+      exten = "USR";
+    }
+  }
+
+  if (elen > 16) {
+    elen = 14;
+    buf[elen] = '#';
+    buf[elen+1] = '1';
+    /* FIXME - enumerate all files in directory and make sure this isn't a duplicate */
+  }
+
+  buf[elen] = '.';
+  strncpy(c64, buf, elen);
+  c64[elen] = '.';
+  c64[elen+1] = 0;
+  strcat(c64, exten);
+  return;
+}
+
+char *dosFindFile(const char *path, int ignoreCase)
+{
+  /* FIXME - remap characters */
+  /* FIXME - search directory if ignoreCase is YES */
+  /* FIXME - map 16 char names to full names */
+  /* FIXME - deal with wildcards */
+  return NULL;
+}
+
+FILE *dosOpenFile(const char *path, int channel, int ignoreCase, char *mode, char **bufPtr)
+{
+  char *fixedPath;
+
+
+  *bufPtr = NULL;
+  if (!strcmp(path, "$") || !strcmp(path, "$0")) {
+    DIR *dir;
+    struct dirent *dp;
+    size_t len;
+    FILE *file;
+    char *buf, *slash;
+    char filename[32];
+    struct stat st;
+    off_t blocks;
+    int bw, nw;
+    char *exten;
+
+
+    /* FIXME - setup buffer */
+    if (!(dir = opendir(".")))
+      return NULL;
+
+    file = open_memstream(bufPtr, &len);
+    fwrite("\001\004\001\001", 4, 1, file);
+    fwrite("\000\000", 2, 1, file); /* Drive number */
+
+    buf = getcwd(NULL, 0);
+    slash = strrchr(buf, '/') + 1;
+    strncpy(filename, slash, 22);
+    filename[22] = 0;
+    for (bw = 0; filename[bw]; bw++)
+      filename[bw] = toupper(filename[bw]);
+    fprintf(file, "\022\"%s\"%*s", filename, 22 - strlen(filename), " ");
+    fputc(0x00, file);
+    free(buf);
+    
+    for (dp = readdir(dir); dp; dp = readdir(dir))
+      if (dp->d_name[0] != '.') {
+	fwrite("\001\001", 2, 1, file);
+	stat(dp->d_name, &st);
+	blocks = (st.st_size + 255) / 256;
+	if (blocks > 65535)
+	  blocks = 65535;
+	for (bw = 1, nw = 9; blocks > nw; bw++, nw = nw * 10 + 9)
+	  ;
+	dosMakeC64Name(dp->d_name, filename);
+	exten = strrchr(filename, '.');
+	nw = exten - filename;
+	*exten = 0;
+	exten++;
+	fprintf(file, "%c%c%*s\"%.16s\"%*s%s%*s%c",
+		(int) blocks & 0xff, (int) (blocks >> 8) & 0xff,
+		5 - bw, " ", filename, 22 - nw, " ", exten, bw, " ", 0x00);
+      }
+
+    fprintf(file, "\001\001%c%cBLOCKS FREE.              %c%c%c",
+	    0xff, 0xff, 0x00, 0x00, 0x00);
+
+    rewind(file);
+    return file;
+  }
+  else if ((fixedPath = dosFindFile(path, ignoreCase)))
+    return fopen(fixedPath, mode);
+  
+  return NULL;
+}
 
 extern void dosHandleIO(int fd)
 {
@@ -34,6 +165,7 @@ extern void dosHandleIO(int fd)
   char filename[1024], exten[10];
   FILE *file;
   char buf[256];
+  char *mem;
   static struct timeval start;
   static int sentLen;
 
@@ -60,15 +192,10 @@ extern void dosHandleIO(int fd)
     switch (sub) {
     case IECOpenCommand:
       fprintf(stderr, "Opening %s\n", data);
-      if (header.len == 1 && data[0] == '$') {
-	/* Directory listing */
-      }
-      else {
-	/* FIXME - make sure filename fits */
-	strcpy(filename, (char *) data);
-	/* Will wait until next command to see if it is a read or
-	   write and check if the file exists */
-      }
+      /* FIXME - make sure filename fits */
+      strcpy(filename, (char *) data);
+      /* Will wait until next command to see if it is a read or
+	 write and check if the file exists */
       break;
 
     case IECCloseCommand:
@@ -109,39 +236,35 @@ extern void dosHandleIO(int fd)
 
   case IECTalkCommand:
     if (sub == IECChannelCommand) {
-      if (chan == 0)
-	strcpy(exten, "PRG");
-      strcat(filename, ".");
-      strcat(filename, exten);
       header.command = 0;
       header.channel = chan;
       header.len = 0;
 	  
-      if ((file = fopen(filename, "r"))) {
-	while ((len = fread(buf, 1, sizeof(buf), file)) > 0) {
-	  if (dlen < header.len + len) {
-	    dlen = header.len + len;
-	    data = realloc(data, dlen);
-	  }
-
-	  memmove(&data[header.len], buf, len);
-	  header.len += len;
-	}
-	fclose(file);
-      }
-	  
-      write(fd, (void *) &header, sizeof(header));
-      if (header.len) {
-	gettimeofday(&start, NULL);
-	sentLen = header.len;
+      if ((file = dosOpenFile(filename, chan, 1, "r", &mem))) {
 	fprintf(stderr, "Sending %s\n", filename);
-	write(fd, data, header.len);
+	gettimeofday(&start, NULL);
+	
+	fseek(file, 0, SEEK_END);
+	header.len = ftell(file);
+	rewind(file);
       }
       else
 	fprintf(stderr, "No such file %s\n", filename);
+	  
+      write(fd, (void *) &header, sizeof(header));
+      
+      if (file) {
+	while ((len = fread(buf, 1, sizeof(buf), file)) > 0)
+	  write(fd, buf, len);
+	fclose(file);
+	if (mem)
+	  free(mem);
+	sentLen = header.len;
+      }
     }
     break;
   }
 
   return;
 }
+
